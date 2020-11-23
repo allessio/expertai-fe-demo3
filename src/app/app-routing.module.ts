1b68106dc88e7710e0b5bd41fc40d61df3cbd53c
Alex Chayka <oleksiy.chayka@eng.it>
2020-11-23 02:17:17 +0100

    init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DocDetailsComponent } from './components/doc-details/doc-details.component';
+import { DocsListComponent } from './components/docs-list/docs-list.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'tutorials', pathMatch: 'full' },
+  { path: 'docs', component: DocsListComponent },
+  { path: 'docs/:id', component: DocDetailsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DocDetailsComponent } from './components/doc-details/doc-details.component';
+import { DocsListComponent } from './components/docs-list/docs-list.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DocDetailsComponent,
+    DocsListComponent,
+    FormsModule,
+    HttpClientModule
   ],
   imports: [
     BrowserModule,
